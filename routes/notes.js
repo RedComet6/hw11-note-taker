387c23f5010f1b8b71c01731d46420fe141d555b
Aidan Chamberlain <asunshine99@gmail.com>
2022-03-01 22:48:22 -0600

    fixed post route to include unique id for each note, added delete route

--- routes/notes.js
+++ routes/notes.js
@@ -1,5 +1,7 @@
 const notes = require("express").Router();
+const { append } = require("express/lib/response");
 const fs = require("fs");
+const { v4: uuidv4 } = require("uuid");
 const notesFile = "./db/db.json";
 
 // GET Route for retrieving submitted notes
@@ -7,6 +9,7 @@ notes.get("/notes", (req, res) => {
     fs.readFile(notesFile, "utf8", (err, data) => res.json(JSON.parse(data)));
 });
 
+// POST route for adding new notes
 notes.post("/notes", (req, res) => {
     const { title, text } = req.body;
 
@@ -14,6 +17,7 @@ notes.post("/notes", (req, res) => {
         const newNote = {
             title,
             text,
+            id: uuidv4(),
         };
 
         fs.readFile(notesFile, "utf8", (err, data) => {
@@ -22,12 +26,34 @@ notes.post("/notes", (req, res) => {
             } else {
                 const parsedData = JSON.parse(data);
                 parsedData.push(newNote);
-                fs.writeFile(notesFile, JSON.stringify(parsedData), (err) => (err ? console.error(err) : console.log("Success!")));
+                fs.writeFile(notesFile, JSON.stringify(parsedData), (err) => (err ? console.error(err) : console.log("Successfully added a note!")));
             }
         });
-        res.json(`Succesfully added a new note`);
+        res.json(`Successfully added a new note`);
     } else {
         res.error("Error in adding new note");
+    }
+});
+
+// delete route
+notes.delete("/notes/:id", (req, res) => {
+    if (req.params.id) {
+        const deletedNoteId = req.params.id;
+
+        fs.readFile(notesFile, "utf8", (err, data) => {
+            if (err) {
+                console.error(err);
+            } else {
+                const parsedData = JSON.parse(data);
+                const deletedNoteIndex = parsedData.findIndex((note) => note.id === deletedNoteId);
+                const deletedNote = parsedData[deletedNoteIndex];
+                parsedData.pop(deletedNote);
+                fs.writeFile(notesFile, JSON.stringify(parsedData), (err) => (err ? console.error(err) : console.log("Successfully deleted a note!")));
+            }
+        });
+        res.json(`Successfully deleted a note!`);
+    } else {
+        res.error("Error in deleting a note");
     }
 });
 
